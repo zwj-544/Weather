8051de2383e4d6f7a5cf7c8a161736f076aa99bc
stone1_zheng <stone1_zheng@asus.com>
2017-04-11 16:59:25 +0800

    add first activity

---
+++ app/src/main/java/com/example/stone/weather/CityActivity.java
@@ -0,0 +1,17 @@
+package com.example.stone.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by czy on 17-4-11.
+ */
+
+public class CityActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_city);
+    }
+}
