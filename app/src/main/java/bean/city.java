1b0462cce06b10098592ad6db7ac1262ac49a244
zwj <zheng_wu_jie@163.com>
2017-04-11 21:34:58 +0800

    add city

---
+++ app/src/main/java/bean/city.java
@@ -0,0 +1,67 @@
+package bean;
+
+/**
+ * Created by zwj on 2017/4/11.
+ */
+public class city {
+    /**
+     * id : CN101010100
+     * cityEn : beijing
+     * cityZh : 北京
+     * countryCode : CN
+     * countryEn : China
+     * countryZh : 中国
+     * provinceEn : beijing
+     * provinceZh : 北京
+     * leaderEn : beijing
+     * leaderZh : 北京
+     * lat : 39.904989
+     * lon : 116.405285
+     */
+
+    private String id;
+    private String cityZh;
+    private String countryZh;
+    private String provinceZh;
+    private String leaderZh;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCityZh() {
+        return cityZh;
+    }
+
+    public void setCityZh(String cityZh) {
+        this.cityZh = cityZh;
+    }
+
+    public String getCountryZh() {
+        return countryZh;
+    }
+
+    public void setCountryZh(String countryZh) {
+        this.countryZh = countryZh;
+    }
+
+    public String getProvinceZh() {
+        return provinceZh;
+    }
+
+    public void setProvinceZh(String provinceZh) {
+        this.provinceZh = provinceZh;
+    }
+
+    public String getLeaderZh() {
+        return leaderZh;
+    }
+
+    public void setLeaderZh(String leaderZh) {
+        this.leaderZh = leaderZh;
+    }
+}
--- app/src/main/java/com/example/stone/weather/CityActivity.java
+++ app/src/main/java/com/example/stone/weather/CityActivity.java
@@ -2,6 +2,19 @@ package com.example.stone.weather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import bean.city;
 
 /**
  * Created by czy on 17-4-11.
@@ -13,5 +26,44 @@ public class CityActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_city);
+        readCityFromAssets();
+    }
+    
+    private void readCityFromAssets(){
+        try {
+            InputStream is = getAssets().open("chinacity.txt");
+            String text = readText(is);
+            handleCities(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleCities(String text) {
+        List<city> cities;
+        Gson gson = new Gson();
+        cities = gson.fromJson(text,new TypeToken<List<city>>(){}.getType());
+        Log.d("zwj","length = "+cities.size());
+        for(int i = 0; i<cities.size(); i++){
+            Log.d("zwj","name ="+cities.get(i).getCityZh());
+        }
+    }
+
+    private String readText(InputStream is) {
+        StringBuffer buffer = new StringBuffer("");
+        try {
+            InputStreamReader reader = new InputStreamReader(is,"utf-8");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String str;
+            while ((str = bufferedReader.readLine()) != null){
+                buffer.append(str);
+                buffer.append("\n");
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buffer.toString();
     }
 }
