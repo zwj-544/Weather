2b4ed3d68e79ade8950d0f7ddc90015b90b45c5d
zwj <zheng_wu_jie@163.com>
2017-04-12 21:42:13 +0800

    add locate city

--- app/src/main/java/com/example/stone/weather/CityActivity.java
+++ app/src/main/java/com/example/stone/weather/CityActivity.java
@@ -6,10 +6,17 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -18,7 +25,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import bean.city;
@@ -27,20 +36,31 @@ import bean.city;
  * Created by czy on 17-4-11.
  */
 
-public class CityActivity extends Activity implements TextWatcher{
+public class CityActivity extends Activity implements TextWatcher, View.OnClickListener, AMapLocationListener, AdapterView.OnItemClickListener{
 
     EditText searchCity;
     ListView searchCityList;
     List<String> cityList = new ArrayList<String>();
+    TextView locate;
+    private AMapLocationClientOption mLocationOption = null;
+    private AMapLocationClient mlocationClient = null;
+    private GridView hotCity;
+    private TextView quickSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_city);
         readCityFromAssets();
+        quickSelect = (TextView) findViewById(R.id.quick_select_city);
         searchCity = (EditText) findViewById(R.id.search_city);
         searchCity.addTextChangedListener(this);
         searchCityList = (ListView) findViewById(R.id.search_city_list);
+        //locate = (TextView) findViewById(R.id.location);
+        //locate.setOnClickListener(this);
+        hotCity = (GridView) findViewById(R.id.hot_city);
+        hotCity.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.hot_city)));
+        hotCity.setOnItemClickListener(this);
     }
     
     private void readCityFromAssets(){
@@ -57,9 +77,7 @@ public class CityActivity extends Activity implements TextWatcher{
         List<city> cities;
         Gson gson = new Gson();
         cities = gson.fromJson(text,new TypeToken<List<city>>(){}.getType());
-        Log.d("zwj","length = "+cities.size());
         for(int i = 0; i<cities.size(); i++){
-            Log.d("zwj","name ="+cities.get(i).getCityZh()+" ,advance ="+cities.get(i).getProvinceZh());
             cityList.add(cities.get(i).getCityZh());
         }
     }
@@ -103,9 +121,70 @@ public class CityActivity extends Activity implements TextWatcher{
             }
             searchCityList.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list));
             searchCityList.setVisibility(View.VISIBLE);
+            hotCity.setVisibility(View.GONE);
+            quickSelect.setVisibility(View.GONE);
         }else {
             searchCityList.setVisibility(View.GONE);
+            hotCity.setVisibility(View.VISIBLE);
+            quickSelect.setVisibility(View.VISIBLE);
         }
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            /*case R.id.location:
+                mlocationClient = new AMapLocationClient(this);//初始化定位参数
+                mLocationOption = new AMapLocationClientOption();//设置定位监听
+                mlocationClient.setLocationListener(this);//设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+                mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);//设置定位间隔,单位毫秒,默认为2000ms
+                mLocationOption.setInterval(2000);//设置定位参数
+                mlocationClient.setLocationOption(mLocationOption);
+                mlocationClient.startLocation();
+                break;*/
+        }
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (amapLocation != null) {
+            if (amapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                amapLocation.getLatitude();//获取纬度
+                amapLocation.getLongitude();//获取经度
+                amapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(amapLocation.getTime());
+                df.format(date);//定位时间
+                amapLocation.getCity();
+                String name=amapLocation.getCity().substring(0,amapLocation.getCity().length()-1);
+                Log.d("zwj:", "city===" + name);
+                mlocationClient.stopLocation();
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError","location Error, ErrCode:"
+                        + amapLocation.getErrorCode() + ", errInfo:"
+                        + amapLocation.getErrorInfo());
+            }
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if(parent.getId() == R.id.hot_city){
+            if(position == 0){
+                mlocationClient = new AMapLocationClient(this);//初始化定位参数
+                mLocationOption = new AMapLocationClientOption();//设置定位监听
+                mlocationClient.setLocationListener(this);//设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+                mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);//设置定位间隔,单位毫秒,默认为2000ms
+                mLocationOption.setInterval(2000);//设置定位参数
+                mlocationClient.setLocationOption(mLocationOption);
+                mlocationClient.startLocation();
+            }else {
+                searchCity.setText(getResources().getStringArray(R.array.hot_city)[position]);
+            }
+        }
     }
 }
